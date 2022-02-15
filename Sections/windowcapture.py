aae6601e58c1dadf9422dc3eed58328efa68abc5
Warner Mentch <admin@warner.blue>
2022-02-15 12:03:43 -0500

    commit on Mater

--- Sections/windowcapture.py
+++ Sections/windowcapture.py
@@ -26,3 +26,71 @@ class WindowCapture(object):
             self.hwnd = win32gui.GetDesktopWindow()
         else:
             self.hwnd = win32gui.FindWindow(None, window_name)
+            if not self.hwnd:
+                raise Exception(f'Window not found: {window_name}')
+
+        # Get the size of the selected window.
+        window_rect = win32gui.GetWindowRect(self.hwnd)
+        self.w = window_rect[2] - window_rect[0]
+        self.h = window_rect[3] - window_rect[1]
+
+        # Cut off the window border/title and crop them out
+        border_pixels = 8
+        titlebar_pixels = 30
+        self.w = self.w - (border_pixels * 2)
+        self.h = self.h - (titlebar_pixels - border_pixels)
+        self.cropped_x = border_pixels
+        self.cropped_y = titlebar_pixels
+
+        # Calculate the window offset to make taking screenshots easier.
+        self.offset_x = window_rect[0] + self.cropped_x
+        self.offset_y = window_rect[1] + self.cropped_y
+
+    # Returns the current screenshot
+    def current_ss(self):
+        return self.screenshot
+
+    # Takes a screenshot of the active/selected window
+    def take_ss(self):
+        # Get window data and make a bitmap 'image'
+        windowContext = win32gui.GetWindowDC(self.hwnd)
+        contextObj = win32ui.CreateDCFromHandle(windowContext)
+        compatibleContext = contextObj.CreateCompatibleDC()
+        bitmap = win32ui.CreateBitmap()
+        bitmap.CreateCompatibleBitmap(contextObj, self.w, self.h)
+        compatibleContext.SelectObject(bitmap)
+        compatibleContext.BitBlt((0, 0), (self.w, self.h), contextObj, (self.cropped_x, self.cropped_y), win32con.SRCCOPY)
+
+        # Next we need to convert the data into a format opencv can process
+        signedIntsArray = bitmap.GetBitmapBits(True)
+        img = np.fromstring(signedIntsArray, dtype = 'uint8')
+        img.shape = (self.h, self.w, 4)
+
+        # Free up computer resources to keep things speedy.
+        contextObj.DeleteDC()
+        compatibleContext.DeleteDC()
+        win32gui.ReleaseDC(self.hwnd, windowContext)
+        win32gui.DeleteObject(bitmap.GetHandle())
+
+        # We need to drop the alpha channel or opencv's template match will throw and error.
+        img = img[...,:3]
+
+        # Make the image C_CONTIGUOUS to avoid errors.
+        # Discussion thread:
+        # https://github.com/opencv/opencv/issues/14866#issuecomment-580207109
+        img = np.ascontiguousarray(img)
+
+        return img
+
+    # Find the window that has a matching name the update the window capture
+    @staticmethod
+    def window_names():
+        def winEnumHandler(hwnd, ctx):
+            print(win32gui.EnumWindows(winEnumHandler, None))
+            if win32gui.IsWindowVisible(hwnd):
+                print(hex(hwnd), win32gui.GetWindowText(hwnd))
+        win32gui.EnumWindows(winEnumHandler, None)
+
+    # This function is used to translate the screenshot image to the pixel position.
+    def screen_position(self, pos):
+        return (pos[0] + self.offset_x, pos[1] + self.offset_y)
--- osuML.py
+++ osuML.py
@@ -1,2 +1,96 @@
-# Main py file for the project.
+# Main file for the project.
 import cv2 as cv
+from cv2 import rectangle
+import numpy as np
+import os
+from time import time
+from Sections.windowcapture import WindowCapture
+from Sections.vision import Vision
+from time import time
+from multiprocessing import Process, Manager
+from multiprocessing.managers import BaseManager
+import multiprocessing
+
+
+def process1(queue):
+    wincap = WindowCapture('opsu!')
+    cascade_circles = cv.CascadeClassifier('cascade/cascade.xml')
+    vision_circles = Vision(None)
+
+    loop_time = time()
+    while(True):
+
+        # Captures the current frame of the game for analysis
+        screenshot = wincap.take_ss()
+
+        # Old template matching code
+        #points = vision_circles.find(screenshot, 0.5, 'rectangles')
+
+        # Detects possible objects in the screenshot.
+        rectangles = cascade_circles.detectMultiScale(screenshot)
+
+        # Highlights said objects.
+        detection_image = vision_circles.draw_rectangles(screenshot, rectangles)
+
+        # Displays the screenshot after the possible objects have been highlighted.
+        cv.imshow('osuML', detection_image)
+
+        # debug the loop rate
+        print('FPS {}'.format(1 / (time() - loop_time)))
+        loop_time = time()
+
+        # press 'q' with the output window focused to exit.
+        # waits 1 ms every loop to process key presses
+        key = cv.waitKey(1)
+        if key == ord('q'):
+            cv.destroyAllWindows()
+            break
+        elif key == ord('d'):
+            cv.imwrite(f'Sections/positive/{loop_time}.jpg', screenshot)
+        elif key == ord('f'):  
+            cv.imwrite(f'Sections/negative/{loop_time}.jpg', screenshot)
+    while True:
+        queue.put(wincap.take_ss())
+
+def process2(queue,display_queue):
+    cascade = cv.CascadeClassifier(cv.data.haarcascades + 'cascade.xml')
+    # assert not cascade.empty()
+    # vision = Vision()
+    flip = 0
+    rectangles = []
+    while True:
+        while not queue.empty():
+            screenshot = queue.get()
+            flip += 1
+            if flip % 15 == 0:
+                rectangles = cascade.detectMultiScale(
+                    screenshot,
+                    scaleFactor=1.1
+                    #minNeighbors=3
+                    #minSize=(40,40)
+                    #flags=cv.CASCADE_SCALE_IMAGE
+                )
+            
+            if len(rectangles) > 0:
+                detection_image = vision.draw_rectangles(screenshot, rectangles)
+            else:
+                detection_image = screenshot
+            display_queue.put(detection_image)
+
+def process3(display_queue):
+    while True:
+        while not display_queue.empty():
+            screenshot = display_queue.get()
+            cv.imshow('Matches', screenshot)
+            key = cv.waitKey(1)
+
+if __name__ == "__main__":
+    queue = multiprocessing.Queue()
+    display_queue = multiprocessing.Queue()
+    p1 = Process(target=process1, args=(queue,))
+    p2 = Process(target=process2, args=(queue,display_queue,))
+    p3 = Process(target=process3, args=(display_queue,))
+    
+    p1.start()
+    p2.start()
+    p3.start()
